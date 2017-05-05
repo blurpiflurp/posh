fd436b79e3bdeaf53ad0492458a508276bfd96b6
Tobias Fredriksson <tobias@Tobiass-MacBook-Pro.local>
2017-05-05 12:49:07 +0200

    Added to code add miners with direction.

--- Factorio/Blueprint.ps1
+++ Factorio/Blueprint.ps1
@@ -1,6 +1,6 @@
-    $datain = "0eNqFkN0KwjAMhd8l1y1sZQ7sq4hItwUJ9I+2E8fou9tOhKGiN4GEnC/nZIVBz+gD2QRyBRqdjSBPK0S6WqXrLC0eQQIlNMDAKlM7gxPNhqPGMQUauXcaITMgO+EdZJvZX0Q0SmuulfE7ochnBmgTJcKnka1ZLnY2A4ZC/qZn4F0sEmfrtYJpGCyl5mrjDSB+Z/hA8fbFKs62BHL3MwY3DHHb7rtOdMdG9Ic+5wejr3Kr"
+    $datain = "0eNqNj8EKwjAQRP9lzltoQi2YXxGRti4aaLel2Yql5N9t4kXQg8dZZt/MbGj7hafZi8Jt8N0oAe60IfibNH266ToxHLzyAII0Q1JBmfuiu3NQRIKXKz/hTDwTWNSr5zcmi/Uiy9DyvBt+AgjTGPafUVJe4pSEFa4wNkb6gtj/IIUxmWJMTLVyf/cxl/DgOWR/XVW2Opa2PtQxvgD/Mlpo"
     
-    function Decode {
+    function Get-BluePrint {
         param($datain)
         $decoded = [System.Convert]::FromBase64String($datain.Substring(1))
         $ms = New-Object System.IO.MemoryStream
@@ -8,10 +8,11 @@
         $ms.Seek(2,0)|Out-Null
         $cs = New-Object System.IO.Compression.DeflateStream($ms,[System.IO.Compression.CompressionMode]::Decompress)
         $sr = New-Object System.IO.StreamReader($cs)
-        return $sr.ReadToEnd()
+        
+        return ($sr.ReadToEnd() | ConvertFrom-Json)
     }
 
-    function Encode {
+    function Set-BluePrint {
         param($decoded)
         $tocompress = ([System.Text.Encoding]::UTF8).GetBytes($decoded.trim())
         $node = & 'C:\Program Files (x86)\nodejs\node.exe' q:\lab\node.js ([System.Convert]::ToBase64String($tocompress))
@@ -31,15 +32,76 @@
         $pos | Add-Member -MemberType NoteProperty -Name y -Value $posy
         $entity | Add-Member -MemberType NoteProperty -Name position -Value $pos
         $entities += $entity
+        Write-Host "Adding $entity"
         return $entities
     }
-    $decoded = Decode -datain $datain
-    $object = $decoded | ConvertFrom-Json
+    Function Create-Miners {
+        param($entities,$direction,$boundary,[switch]$walls,[switch]$laserturrets,[switch]$gunturrets,[switch]$flameturrets)
+        $freespace = 1;
+        if($walls) {
+            $freespace = 8;
+        }
+        if($direction -eq $null) {$direction = 2}
+        $pos = $boundary.TopRight
+        $pos.x -= ($freespace+2)
+        $origy = $boundary.TopRight.y
+        Write-Host $pos
+        while($pos.x -lt $boundary.BottomLeft.x) {
+            
+            
+            while($pos.y -gt $boundary.BottomLeft.y) {
+                $entities = Add-Entity -entities $entities -name "electric-mining-drill" -posx $pos.x -posy $pos.y -direction $direction
+                $pos.y -= 4
+                Write-Host "Y is $($pos.y) and waiting for it to be $($boundary.BottomLeft.y)"
+            }
+            $pos.x += 4
+            $pos.y = $origy
+            Write-Host "X is $($pos.x) and waiting for it to be $($boundary.BottomLeft.x) (y is now $($pos.y) should be $($origy))"
+            if($direction -eq 2) {
+                $direction = 6
+            } else {
+                $direction = 2
+            }
+        }
+        return $entities
+    }
+    function Get-Boundary {
+        param([psobject]$entities,[int]$extend)
+        $x = 0;
+        $y = 0;
+        $x2 = 0;
+        $y2 = 0;
+        $i=0;
+        foreach($entity in $entities) {
+            if($entity.position.x -lt 0 -and $entity.position.x -lt $x) { $x = $entity.position.x; write-host "New lowest x:$x for $($entity.name) ($i)" }
+            elseif($entity.position.x -gt 0 -and $entity.position.x -gt $x2) { $x2 = $entity.position.x;write-host "New highest x:$x2 for $($entity.name) ($i)" }
+            if($entity.position.y -gt 0 -and $entity.position.y -gt $y) { $y = $entity.position.y;write-host "New highest y:$y for $($entity.name) ($i)" }
+            elseif($entity.position.y -lt 0 -and $entity.position.y -lt $y2) { $y2 = $entity.position.y;write-host "New lowest y:$y2 for $($entity.name) ($i)" }
+            $i++
+        }
+        if($extend -gt 0) {
+            $x -= $extend
+            $y += $extend
+            $x2 += $extend
+            $y2 -= $extend
+        }
+        $obj = New-Object -TypeName pscustomobject
+        $TopRight = New-Object -TypeName pscustomobject
+        $BottomLeft = New-Object -TypeName pscustomobject
+        $TopRight|Add-Member -MemberType NoteProperty -Name x -Value $x
+        $TopRight|Add-Member -MemberType NoteProperty -Name y -Value $y
+        $BottomLeft|Add-Member -MemberType NoteProperty -Name x -Value $x2
+        $BottomLeft|Add-Member -MemberType NoteProperty -Name y -Value $y2
+        $obj|Add-Member -MemberType NoteProperty -Name TopRight -Value $TopRight
+        $obj|Add-Member -MemberType NoteProperty -Name BottomLeft -Value $BottomLeft
+        return $obj
+    }
+    $object = Get-BluePrint -datain $datain
     $entities = $object.blueprint.entities
-    $entities = Add-Entity -entities $entities -name "roboport" -posx 10 -posy 10
-    $object.blueprint.entities = $entities
-    $json = ($object|ConvertTo-Json -Depth 10).Replace("`n","").Replace(" ","")
-
-    $enc = Encode -decoded $json
-    Write-Host $enc
+    $boundary = Get-Boundary -entities $entities
+    $miners = Create-Miners -entities $entities -direction 2 -boundary $boundary
+    $newblueprint = $object
+    $newblueprint.blueprint.entities = $miners
+    $json = ($newblueprint|ConvertTo-Json -Depth 10).Replace("`n","").Replace(" ","")
+    $enc = Set-BluePrint -decoded $json
     $enc|clip
